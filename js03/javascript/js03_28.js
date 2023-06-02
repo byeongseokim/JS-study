bcc430fda48ad8a044e75fa5a23d8202167aa222
byeongseokim <kbs970318@gmil.com>
2023-06-02 17:17:33 +0900

    객체 이해하기1

---
+++ js03/javascript/js03_28.js
@@ -0,0 +1,18 @@
+let family = {
+    'address' : 'Seoul' , 
+    members: {},
+    addFamily: function(age, name, role) {
+        this.members[role] = {
+            age: age,
+            name: name
+        };
+    },
+    getHeadcount: function() {
+        return Object.keys(this.members).length;
+    }
+};
+
+family.addFamily(30, 'chloe', 'aunt');
+family.addFamily(3, 'lyn, ','niece');
+family.addFamily(10, 'dangdagi', 'dog');
+console.log(family.getHeadcount());
