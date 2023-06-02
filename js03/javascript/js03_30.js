28f7b89e1281c1b1a94b424e465f9bb1698f145f
byeongseokim <kbs970318@gmil.com>
2023-06-02 17:18:05 +0900

    ES6의 향상된 객체 문법 알아보기 - 단축 속성명

---
+++ js03/javascript/js03_30.js
@@ -0,0 +1,15 @@
+let address = 'seoul';
+let members =  {};
+;let addFamily = function(age, name, role) {
+    this.members[role] = {age, name};
+};
+let getHeadcount = function() {
+    return Object.keys(this.members).length;
+};
+
+let family = {address, members, addFamily, getHeadcount};
+
+family.addFamily(30, "chloe", "aunt");
+family.addFamily(3, "lyn, ", "niece");
+family.addFamily(10, "dangdagi", "dog");
+console.log(family.getHeadcount());
