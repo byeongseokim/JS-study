45ba22ac0bd6c9068eb5b2afc0d166bf717ffe5d
byeongseok <kbs970318@gmail.com>
2023-06-05 22:50:02 +0900

    함수 이해하기

---
+++ js04/javascript/js04_34.js
@@ -0,0 +1,10 @@
+let greeting_expression = function(name) {
+    console.log('Hi, ' + name);
+}
+
+function greeting_declaration(name) {
+    console.log('Hi, ' + name);
+}
+
+greeting_expression('Chloe');
+greeting_declaration('Chloe');
