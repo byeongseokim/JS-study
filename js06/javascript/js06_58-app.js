dd95e76ce4a895556e0b979524e0df19ca777e93
byeongseok <kbs970318@gmail.com>
2023-06-08 23:13:15 +0900

    모듈을 여러 이름으로 내보내고 가져오기

---
+++ js06/javascript/js06_58-app.js
@@ -0,0 +1,8 @@
+import {add, person, version} from './js06_58-hello.js';
+
+const result = add(1, 2);
+const harin = new person('하린');
+
+console.log(result);
+console.log(harin.name);
+console.log(version);
---
+++ js06/javascript/js06_58-hello.js
@@ -0,0 +1,15 @@
+export const version = "v1.0";
+
+export var personA = {
+  name: "a",
+};
+
+export function add(a, b) {
+  return a + b;
+}
+
+export class person {
+  constructor(name) {
+    this.name = name;
+  }
+}
