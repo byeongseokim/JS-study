61f94fb1a20114f7b1b68869f8f51aa58e90a8e5
byeongseok <kbs970318@gmail.com>
2023-06-10 16:14:09 +0900

    문자열 자르기(substr)

---
+++ js07/javascript/js07_70.js
@@ -0,0 +1,8 @@
+const sentence = 'Wakanda Forever!!!';
+console.log(sentence.substring(8));
+console.log(sentence.substring(8, 7));
+console.log(sentence.substring(0));
+console.log(sentence.substring(-10));
+console.log(sentence.substring(0, -3));
+console.log(sentence.substring(30));
+console.log(sentence.substring(0, 30));
